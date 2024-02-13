d0b662238c7cda701ac4a1936ff8e3c261c6d42e
iAmSherm2 <trace.anderson@students.fhu.edu>
2024-02-13 10:19:34 -0600

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,8 +1,23 @@
 #Revmoe pass and complete the cat class
 class Cat():
-    pass
+    def __init__(self):
+        self.name = "unknown"
+        self.age = 0
+    
+    def speak(self):
+        return("Meow")
+
 
 
 #Create objects here
+garfield = Cat()
+garfield.name = "Garfield"
+garfield.age = 50
+
+
+stella = Cat()
+stella.name = "Stella"
+stella.age = 7
+
 #These should NOT be indented inside the class
 
